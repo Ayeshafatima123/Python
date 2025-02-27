cf5958635dc25cc7bf1dd646998a86aa965afefc
Ayeshafatima123 <160357945+Ayeshafatima123@users.noreply.github.com>
2025-02-27 23:17:16 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+print("hello world from AYESHA"),
+
+def giaic():
+    print("hello world from AYESHA"),
+    
+
+                        #"" Data types"
+                        # There are many data tyoes of Python 
+        # for example             
+#  string
+# integer
+# float
+# boolean
+# list
+# tuple
+# dict
+# set
+# None
+# frozen set
+
+# /string
+
+name :str = "Governor Initiative for AI Computing"
+print("GIAIC stands for") 
+
+
+# Special keyword
+import keyword
+print ("keyword are:")
+print (keyword.kwlist)
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
